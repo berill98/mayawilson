f529e38647ba55d2cca1f1356c7a6e72b5efe198
berill98 <berill0407@gmail.com>
2022-10-30 17:59:12 +0000

    Customize admin

--- packages/admin.py
+++ packages/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Package
 
-admin.site.register(Package)
+class PackageAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'price',
+        'description',
+        'duration',
+        'photo_count',
+    )
+
+    ordering = ('price',)
+
+admin.site.register(Package, PackageAdmin)
