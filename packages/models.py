a2577d06f4fb82a9c32737cf8b2b1d359600d6d8
berill98 <berill0407@gmail.com>
2022-10-30 17:07:04 +0000

    Add Packages app and Package model

---
+++ packages/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Package(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    duration = models.IntegerField(blank=True, null=True)
+    photo_count = models.IntegerField(blank=True, null=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
