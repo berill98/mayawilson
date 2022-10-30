5bdf309a48c2f1355dc2d14af25c65fd344233c7
berill98 <berill0407@gmail.com>
2022-10-30 22:19:21 +0000

    Add package detail functionality

--- packages/views.py
+++ packages/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Package
 
-# Create your views here.
 def all_packages(request):
     """ A view to show all packages """
 
@@ -12,3 +11,14 @@ def all_packages(request):
     }
 
     return render(request, 'packages/packages.html', context)
+
+def package_detail(request, package_id):
+    """ A view to show individual package details """
+
+    package = get_object_or_404(Package, pk=package_id)
+
+    context = {
+        'package': package,
+    }
+
+    return render(request, 'packages/package_detail.html', context)
