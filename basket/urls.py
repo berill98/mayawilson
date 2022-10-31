79a39223708907e4f7123bf3b8f2f27c8c861a27
berill98 <berill0407@gmail.com>
2022-10-31 22:34:13 +0000

    Update the remove from basket function

--- basket/urls.py
+++ basket/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
-    path('add/<package_id>', views.add_to_basket, name='add_to_basket')
+    path('add/<package_id>', views.add_to_basket, name='add_to_basket'),
+    path('remove/<package_id>/', views.remove_from_basket, name='remove_from_basket'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -8,7 +8,7 @@ def view_basket(request):
 
 
 def add_to_basket(request, package_id):
-    """ Add a package to the shopping bag """
+    """ Add a package to the basket """
 
     quantity = int(request.POST.get('quantity')),
     basket = request.session.get('basket', {})
@@ -17,3 +17,21 @@ def add_to_basket(request, package_id):
 
     request.session['basket'] = basket
     return render(request, 'basket/basket.html')
+
+
+def remove_from_basket(request, package_id):
+    """Remove the item from the basket"""
+
+    try:
+        package = get_object_or_404(Package, pk=package_id)
+        basket = request.session.get('basket', {})
+
+        bag.pop(package_id)
+        messages.success(request, f'Removed {package.name} from your basket')
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
