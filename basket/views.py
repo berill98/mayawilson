2a46b6adb5a40e84e18a6b36fb2b4f83561e16af
berill98 <berill0407@gmail.com>
2022-10-31 20:51:56 +0000

    Add basic add to basket functionality

--- basket/views.py
+++ basket/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 
+
 def view_basket(request):
     """ A view that renders the basket contents page """
 
+    return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, package_id):
+    """ Add a package to the shopping bag """
+
+    quantity = int(request.POST.get('quantity')),
+    basket = request.session.get('basket', {})
+
+    basket[package_id] = quantity
+
+    request.session['basket'] = basket
+    print(request.session['basket'])
     return render(request, 'basket/basket.html')
