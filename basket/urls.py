9b758c2cfffa7307a3b20de30e1246fd7d6507b2
berill98 <berill0407@gmail.com>
2022-11-05 18:48:24 +0000

    Remove basket app

--- basket/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.view_basket, name='view_basket'),
-    path('add/<package_id>', views.add_to_basket, name='add_to_basket'),
-    path('remove/<package_id>/', views.remove_from_basket, name='remove_from_basket'),
-]
