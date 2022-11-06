a1d4e4c4d5689109d437ebb8709063e568a05fb2
berill98 <berill0407@gmail.com>
2022-11-06 09:07:18 +0000

    Create Contact models, urls and views

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    email_date = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    surname = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    subject = models.CharField(max_length=254, null=True, blank=True)
+    query = models.TextField(null=True, blank=True)
+    hearfrom = models.CharField(max_length=254, null=True, blank=True)
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.core.mail import send_mail
+from django.contrib import messages
+from django.conf import settings
+from django.template.loader import render_to_string
+from .forms import ContactForm
 
-# Create your views here.
+
+def contact(request):
+    """ A view to return the contact page """
+    if request.method == "POST":
+        form_data = {
+            'first_name': request.POST['first_name'],
+            'surname': request.POST['surname'],
+            'email': request.POST['email'],
+            'subject': request.POST['subject'],
+            'query': request.POST['query'],
+           'hearfrom': request.POST['hearfrom']
+        }
+        contact_form = ContactForm(form_data)
+
+    contact_form = ContactForm()
+    template = 'contact/contact.html'
+    context = {
+        'contact_form': contact_form,
+    }
+
+    return render(request, template, context)
+        
+    #send_confirmation_email(user_contact)
+
+
+def send_confirmation_email(user_contact):
+    """Send the user a confirmation email"""
+    cust_email = user_contact.email
+    subject = 'Thank you for your message to Maya Wilson Photography'
+    body = render_to_string(
+            'contact/confirmation_emails/confirmation_email_body.txt',
+            {'contact': user_contact})
+
+    send_mail(
+        subject,
+        body,
+        settings.DEFAULT_FROM_EMAIL,
+        [cust_email]
+    )
