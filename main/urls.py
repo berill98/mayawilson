81c7f320c04fbe1f4a1e91ccd77572bc940f9250
berill98 <berill0407@gmail.com>
2022-11-06 09:08:19 +0000

    Add contact path to main urls

--- main/urls.py
+++ main/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('checkout/', include('checkout.urls')),
     path('about/', include('about.urls')),
+    path('contact/', include('contact.urls')),
 ]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
